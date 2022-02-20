1e0e23a81331d46e1db99daa9cb66b5862419674
Frank Mckenna <fmckenna56330@gmail.com>
2022-02-20 10:32:09 +0000

    chore: remove notification model

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,5 @@
 const { Op, Sequelize } = require("sequelize");
 const db = require("../db");
-const Message = require("./message");
 
 const Conversation = db.define("conversation", {
   onlineUserId: {
